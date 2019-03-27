7a5b026a2f56c390d42e1505367141fe6792e2f2
“Onixmpc” <onixmpc@gmail.com>
2019-03-27 13:23:22 -0400

    yo que se

--- views/Login/registro.php
+++ views/Login/registro.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" type="text/css" href="registro.css">
+    <link rel="stylesheet" type="text/css" href="../dist/css/registro.css">
     <title>Document</title>
 </head>
 <body>
@@ -66,6 +66,6 @@
   </form>
   <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js" crossorigin="anonymous"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js" crossorigin="anonymous"></script>
-  <script src="registro.js"></script>
+  <script src="../dist/js/registro.js"></script>
 </body>
 </html>
