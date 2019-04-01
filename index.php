45bed1c67a0d1a16bf707a285e639af53a4b4eef
Brandor Medina <41803054+BrandorMedina@users.noreply.github.com>
2019-04-01 01:27:00 -0400

    Cambio de fuentes

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body class=" " id="back">
-      <nav class="navbar navbar-expand-lg navbar-light bg-white align-middl mb-0 h6" style="height: 50px; padding: 35px;">
+      <nav class="navbar navbar-expand-lg navbar-light bg-white align-middl mb-0 h6" id="navbar">
         <a class="navbar-brand" href="">
 
             <img src="views/dist/img/logo1.png" width="70px" alt="">
