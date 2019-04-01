de2a5b32d22cb24a5fbf37aae5675120291caa0d
Aaron <aaronsenogara@gmail.com>
2019-03-31 21:33:53 -0400

    Update dashboard.php

--- views/Dashboard/dashboard.php
+++ views/Dashboard/dashboard.php
@@ -4,63 +4,63 @@
     <meta charset="utf-8">
     <title>Page Title</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="../bootstrap/css/bootstrap.css">
     <link rel="stylesheet" href="../dist/css/AdminLTE.css">
+    <link rel="stylesheet" href="../dist/css/skins/_all-skins.css">
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+
 
 </head>
-<body class="skin-blue layout-top-nav">
 
-    <header class="main-header ">
-        <nav class="navbar navbar-static-top ">
-        <div class="container-fluid">
-            <div class="navbar-header">
-            <a href="../../index2.html" class="navbar-brand"><b>Admin</b>LTE</a>
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse">
-                <i class="fa fa-bars"></i>
-            </button>
-            </div>
+    <body class="skin-green-light layout-top-nav">
 
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="navbar-collapse">
-            <ul class="nav navbar-nav">
-                <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
-                <li><a href="#">Link</a></li>
-                <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <span class="caret"></span></a>
-                <ul class="dropdown-menu" role="menu">
-                    <li><a href="#">Action</a></li>
-                    <li><a href="#">Another action</a></li>
-                    <li><a href="#">Something else here</a></li>
-                    <li class="divider"></li>
-                    <li><a href="#">Separated link</a></li>
-                    <li class="divider"></li>
-                    <li><a href="#">One more separated link</a></li>
-                </ul>
-                </li>
-            </ul>
-            <form class="navbar-form navbar-left" role="search">
-                <div class="form-group">
-                <input type="text" class="form-control" id="navbar-search-input" placeholder="Search">
+        <header class="main-header ">
+            <nav class="navbar navbar-static-top ">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                <a href="dashboard.php" class="navbar-brand"><b>Prestame</a>
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse">
+                    <i class="fa fa-bars"></i>
+                </button>
                 </div>
-            </form>
-            <ul class="nav navbar-nav navbar-right">
-                <li><a href="#">Link</a></li>
-                <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <span class="caret"></span></a>
-                <ul class="dropdown-menu" role="menu">
-                    <li><a href="#">Action</a></li>
-                    <li><a href="#">Another action</a></li>
-                    <li><a href="#">Something else here</a></li>
-                    <li class="divider"></li>
-                    <li><a href="#">Separated link</a></li>
+
+                
+                <div class="collapse navbar-collapse" id="navbar-collapse">
+                <ul class="nav navbar-nav">
+                    <li><a href="#"> Solicitar <span class="sr-only">(current)</span></a></li>
+                    <li><a href="#">Opcion 2</a></li>    
                 </ul>
-                </li>
-            </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Mi Cuenta <span class="caret"></span></a>
+                    <ul class="dropdown-menu" role="menu">
+                        <li><a href="#">Configuracion</a></li>
+                        <li><a href="#">Ayuda</a></li>
+                        <li class="divider"></li>
+                        <li><a href="#">Cerrar Sesión</a></li>
+                    </ul>
+                    </li>
+                </ul>
+                </div>
+                <!-- /.navbar-collapse -->
             </div>
-            <!-- /.navbar-collapse -->
-        </div>
-        <!-- /.container-fluid -->
-        </nav>
-    </header>
+            <!-- /.container-fluid -->
+            </nav>
+        </header>
+
+           <div class="info-box bg-blue">
+            <span class="info-box-icon"><i class="fa fa-comments-o"></i></span>
+            <div class="info-box-content">
+                <span class="info-box-text">Primer Prestamo</span>
+                <span class="info-box-number">20,000 $</span>
+                <div class="progress">
+                <div class="progress-bar" style="width: 70%"></div>
+                </div>
+                <span class="progress-description">
+                70% Increase in 30 Days
+                </span>
+            </div>
+            </div>
 
             
 
