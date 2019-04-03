c0f2c778ec4a2c61d53280a6bfdf3a62cc2759d7
Brandor Medina <41803054+BrandorMedina@users.noreply.github.com>
2019-04-03 18:30:15 -0400

    favicon

--- views/Login/registro.php
+++ views/Login/registro.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="shortcut icon" type="image/png" href="views/dist/img/logo1.png">
+    <link rel="shortcut icon" type="image/png" href="../dist/img/logo1.png">
     <link rel="stylesheet" type="text/css" href="../dist/css/registro.css">
     <title>Document</title>
 </head>
