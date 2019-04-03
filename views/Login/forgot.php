fd95f26e22b8b3ca198e608dbcc86d1edd0bbdcc
Brandor Medina <41803054+BrandorMedina@users.noreply.github.com>
2019-04-03 18:28:55 -0400

    favicon
    Login
    registro
    reset
    forgot

--- views/Login/forgot.php
+++ views/Login/forgot.php
@@ -4,6 +4,7 @@
 	<meta charset="utf-8">
 	<meta name="author" content="Kodinger">
 	<title>Clave olvidada</title>
+	<link rel="shortcut icon" type="image/png" href="views/dist/img/logo1.png">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 	<link rel="stylesheet" type="text/css" href="../dist/css/my-login.css">
 </head>
