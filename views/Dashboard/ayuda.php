b93d60884d2c4b54055b7aabe845cbd15e569243
Aaron <aaronsenogara@gmail.com>
2019-04-04 23:16:50 -0400

    Cambios y nueva vistas agregadas

--- views/Dashboard/ayuda.php
+++ views/Dashboard/ayuda.php
@@ -2,56 +2,48 @@
 <html>
 <head>
     <meta charset="utf-8">
-    <title>Vista General2</title>
+    <title>Ayuda</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="../bootstrap/css/bootstrap.css">
     <link rel="stylesheet" href="../dist/css/AdminLTE.css">
     <link rel="stylesheet" href="../dist/css/skins/_all-skins.css">
     <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
-</head>
 
-    <body class="skin-green-light layout-top-nav">
+</head>
+<body class="skin-green-light layout-top-nav">
     <header class="main-header">
      <?php include "navbardentro.php"; ?>
      </header>
-        
 
-        <div class="col-md-4">
-            <div class="box box-solid box-warning">
+     <div class="col-md-4 center-block">
+            <div class="box box-danger">
                 <div class="box-header">
-                    <h3 class="box-title">Prestamos por cobrar</h3>
+                    <h3 class="box-title">Contactanos</h3>
                 </div>
                     <div class="box-body">
-                    Cuerpo
+                    <h4>Correo electronico:</h4>
+                    <p href="contacto@mail.com">contacto@mail.com<p>
+
+                    <h4>Telefono:</h4>
+                    <p> 809-555-5555
+
                     </div>
             </div>
         </div>
 
-        <div class="col-md-4">
-            <div class="box box-solid box-danger">
+        <div class="col-md-4 center-block">
+            <div class="box box-danger">
                 <div class="box-header">
-                    <h3 class="box-title">Prestamos pasados de fecha</h3>
+                    <h3 class="box-title">preguntas y respuestas</h3>
                 </div>
                     <div class="box-body">
-                    Cuerpo
+                    Pregunta?
+                    <p> Respuesta
                     </div>
             </div>
         </div>
 
-        <div class="col-md-4">
-            <div class="box box-solid box-success">
-                <div class="box-header">
-                    <h3 class="box-title">Prestamos cobrados</h3>
-                </div>
-                    <div class="box-body">
-                    Cuerpo
-                    </div>
-            </div>
-        </div>
-
-            
-
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
---
+++ views/Dashboard/navbardentro.php
@@ -0,0 +1,32 @@
+            <nav class="navbar navbar-static-top ">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                <a href="dashboard.php" class="navbar-brand"><b>Administrador</a>
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse">
+                    <i class="fa fa-bars"></i>
+                </button>
+                </div>
+
+                
+                <div class="collapse navbar-collapse" id="navbar-collapse">
+                <ul class="nav navbar-nav">
+                    <li><a href="#"> Tasa de interes <span class="sr-only">(current)</span></a></li>
+                    <li><a href="#">Opcion 2</a></li>    
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Mi Cuenta <span class="caret"></span></a>
+                    <ul class="dropdown-menu" role="menu">
+                        <li><a href="../Dashboard/configadministrador.php">Configuracion</a></li>
+                        <li><a href="../Dashboard/ayuda.php">Ayuda</a></li>
+                        <li class="divider"></li>
+                        <li><a href="../Login/index.php">Cerrar Sesión</a></li>
+                    </ul>
+                    </li>
+                </ul>
+                </div>
+                <!-- /.navbar-collapse -->
+            </div>
+            <!-- /.container-fluid -->
+            </nav>
+
