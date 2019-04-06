99bbdc49a44b9dad3eb8ecbd980463744bf8b41d
“Onixmpc” <onixmpc@gmail.com>
2019-04-05 21:27:55 -0400

    Algunos comentarios

--- model/Conection_BD.php
+++ model/Conection_BD.php
@@ -5,19 +5,19 @@ const DB_CHARACTER = 'utf8';
 
 abstract class Conection_BD {
 
-private static $db_usuario = '';
-private static $db_pass = '';
+private static $db_usuario = ''; # <-- Usuario que tiene acceso a la BD.
+private static $db_pass = '';  # <-- password del usuario.
 private static $db_servidor = DB_SERVIDOR;
-private static $db_name = 'prestamos';
+private static $db_name = ''; # <-- Nonbre de la BD.
 private static $db_charset= DB_CHARACTER;
 
-#Conexion con la BD
+# CONEXION A LA BD
 public function conectar (){
 
     try {
         $dsn = "mysql:host=".self::$db_servidor. ";dbname=".self::$db_name;
-
         $pdo = new PDO($dsn, self::$db_usuario, self::$db_pass);
+
         # Asignar un caracter
         $pdo->exec("SET CHARACTER SET".self::$db_charset);
 
