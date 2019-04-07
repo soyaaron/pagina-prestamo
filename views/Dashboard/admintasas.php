9d6400ebaf5f8748246206ae2c5701fc9c1b61cb
Aaron <aaronsenogara@gmail.com>
2019-04-07 16:57:55 -0400

    arrego de campo para subir tasas

--- views/Dashboard/admintasas.php
+++ views/Dashboard/admintasas.php
@@ -20,15 +20,27 @@
         <div class="row">
             <div class="col-md-4">
 
+            <div class="box box-danger">
+                    <div class="box-header">
+                        <h3 class="box-title">Actualizar tasas</h3>
+                    </div>
+                        <div class="box-body">
+                  
+                        <div class="input-group">
+                            <input type="number" class="form-control" placeholder="Ingrese cantidad deseada">
+                            <span class="input-group-addon" id="basic-addon2">%</span>
+                            </div>
+
+                            <button  onclick="alertatasa()" class="btn btn-default" type="button" id="aceptartasa">Aceptar</button>
+
+                         </div>
+
+                </div>  
 
                     <div class="box box-solid box-success">
                         <div class="box-header">
                             <h3 class="box-title">Historial de tasas</h3>
-                            <div class="box-tools pull-right">
-                            <button type="button" class="btn btn-box-tool" data-widget="collapse">
-                            <i class="fa fa-minus">Colapsar</i>
-                            </buttom>
-                            </div>
+                           
                         </div>
                             <div class="box-body">
                             
@@ -42,24 +54,24 @@
                                             <tr>
                                                 <td>Un dia </td>
                                                 <td>un numero</td>
-                                               
-
+                                        
                                             </tr>
                                         </tbody>
-                                
                                     </table>
+                                </div>
+                             </div>
 
-                            </div>
-                    </div>
-            </div> 
+                
         </div>
-
+        
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-        <script src="/themes/AdminLTE/bower_components/jquery/dist/jquery.min.js"></script>
-        <script src="/themes/AdminLTE/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>    
         <script src="../dist/js/app.js"></script>
-
+<script>
+    function alertatasa(){
+        confirm("Esta seguro que desea aceptar?");
+    }
+    </script>
      </body>
 </html>
