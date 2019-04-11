0182ab469cb2ebf2791540ff15c25f6ea23d6588
Brandor Medina <41803054+BrandorMedina@users.noreply.github.com>
2019-04-11 14:18:29 -0400

    repair desing registro and add back botton

--- views/Login/registro.php
+++ views/Login/registro.php
@@ -20,14 +20,12 @@
     </ul>
     <!-- fieldsets -->
     <fieldset>
-        <a href="index.php" class="float-right">
-            Atras
-            </a>
       <h2 class="fs-title">Crea tu cuenta</h2>
       <h3 class="fs-subtitle">Paso 1</h3>
       <input type="text" name="email" placeholder="Email" />
       <input type="password" name="pass" placeholder="Password" />
       <input type="password" name="cpass" placeholder="Confirm Password" />
+      <a href="index.php"><input type="button" name="" class="previous action-button" value="Anterior" /></a> 
       <input type="button" name="next" class="next action-button" value="Siguiente" />
     </fieldset>
     <fieldset>
@@ -73,7 +71,8 @@
       <input type="text" name="phone" placeholder="Phone" />
       <textarea name="address" placeholder="Address"></textarea>-->
       <input type="button" name="previous" class="previous action-button" value="Anterior" />
-      <input type="submit" name="submit" class="btn btn-primary" value="Enviar" />
+      <input type="submit" name="submit" class="btn btn-primary text-white active" 
+      style="width: 100px; font-weight: bold; color: white; border: 0 none; border-radius: 1px; cursor: pointer; padding: 10px 5px; margin: 10px 5px;" value="Enviar" />
     </fieldset>
   </form>
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
