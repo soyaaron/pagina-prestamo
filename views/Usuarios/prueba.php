ae12777175ba85f8697486f33a0a4ed4bad5b274
reimy <reimyreymm@gmail.com>
2019-04-12 18:41:15 -0400

    Finalizacion de las vistas

--- views/Usuarios/prueba.php
+++ views/Usuarios/prueba.php
@@ -3,111 +3,219 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <link rel="shortcut icon" type="image/png" href="../dist/img/logo1.png">
+ <link rel="shortcut icon" type="image/png" href="../dist/img/logo1.png">
   <title>Sistema de financiamiento</title>
 
 <script defer src="https://use.fontawesome.com/releases/v5.8.1/js/all.js" integrity="sha384-g5uSoOSBd7KkhAMlnQILrecXvzst9TdC09/VM+pjDTCM+1il8RHz5fKANTFFb+gQ" crossorigin="anonymous"></script>
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.6 -->
-  <link rel="stylesheet" href="views/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
   <!-- Theme style -->
-  <link rel="stylesheet" href="views/dist/css/AdminLTE.min.css">
-<link rel="stylesheet" href="views/dist/css/skins/skin-blue.min.css">
+  <link rel="stylesheet" href="../dist/css/AdminLTE.min.css">
+<link rel="stylesheet" href="../dist/css/skins/skin-blue.min.css">
+<link rel="stylesheet" href="../dist/css/template.css">
 </head>
-<body class="hold-transition skin-blue sidebar-mini" href="views/dist/css/skins/skin-blue.min.css">
-
- <aside class="main-sidebar">
+<body class="hold-transition skin-blue sidebar-mini" href="../dist/css/skins/skin-blue.min.css">
+<aside class="main-sidebar">
 
   
-    <section class="sidebar"  >
+<section class="sidebar"  >
 
   
     
         
-      </div>
- 
+</div>
+<style> 
 
-      <ul class="sidebar-menu" >
-        <li class="header" style="margin-right:10px;"><h1> <label style="margin-right:20px;">Impekable</label> </h1> </li>
-    
-        <li class="active"><a href="/SistemaPHP/views/historial.php"><i class="fas fa-home"></i>Home-calculadora </a></li>
-        <li><a href="#"><i class="fas fa-receipt"></i> <span>Historial de prestamos</span></a></li>
-         <li><a href="/SistemaPHP/views/template.php"><i class="fas fa-envelope"></i> <span>Solicitud de prestamos</span></a></li>
-        <li class="treeview">
-          <a href="#"><i class="fas fa-user"></i></i> <span>Perfil usuario </span>
-   
-        </li>
-      </ul>
-   
-    </section>
+.sidebar{
+	
+	
+	width:100%;
+	height: 10%;
+	position: absolute;     
+}
+.sidebar h2{
+	
+	
+	text-align: center;
+	padding: 0;
+	margin:0;
+ border-color: red;
+ color:white;
+}
+body {
+	height: 100%;
+	background-image: url("../img/fondo-login.jpg");
+	background-size: cover;
+	background-attachment: fixed;
+	
+}
+body{
+	
+	font-family: Arial;
+}
+
+#main-container{
+	margin: 50px auto;
+	width: 520px;
+}
+
+table{
+	background-color: white;
+	text-align: left;
+	border-collapse: collapse;
+  width: 5%;
+  height:5%;
+}
+
+th, td{
+	padding: 20px;
+}
+
+thead{
+	background-color:#246355;
+	border-bottom: solid 5px #0F362D;
+	color: white;
+}
+
+tr:nth-child(even){
+	background-color: #ddd;
+}
+
+tr:hover td{
+	background-color: #369681;
+	color: white;
+}
+
+</style>
+
+<ul class="sidebar-menu" >
+  <li class="header" style="margin-right:10px;;"><h2> Impekable </h2> </li>
+
+  <li class="active"><a href="/SistemaPHP/views/historial.php"><i class="fas fa-home"></i>Home-calculadora </a></li>
+  <li><a href="prueba.php"><i class="fas fa-receipt"></i> <span>Historial de prestamos</span></a></li>
+   <li><a href="solicitud-prestamo.php"><i class="fas fa-envelope"></i> <span>Solicitud de prestamos</span></a></li>
+  <li class="treeview">
+    <a href="perfil.php"><i class="fas fa-user"></i></i> <span>Perfil usuario </span>
+
+  </li>
+</ul>
+
+</section>
 
   </aside>
 
 
   <div class="content-wrapper" style="margin-right:21%; margin-left:25% ; Margin-top:3%; height:20px; ">
-  <style>
-h2{
- color:black;
- margin-left:5%;
-}
+    <style>
+    
 h3{
  color:black;
  margin-left:5%;
 }
+
+ h3{
+	color:black;
+	size: 15px;
+	font-size: 20px;
+	text-shadow: unset;
+	margin-right: 20px;
+  
+  
+
+}
+
+
+  h2{
+    margin-left:30px;
+    color:Black;
+font-family:Copperplate Gothic Light; 
+
+}
+
+
+input{
+
+	border: 2px solid ;
+	border-radius: 4px;
+  outline: none;
+}
+
 label{
-  color:black;
+font-size:14px;
+color:black;
+
 }
 .main-form{
     width: 450px;
     margin: auto;
 }
+img{
+
+	content:12px;
+	border-image:40px;
+	height:60px;
+
+  margin-left:5%;
+  margin-right:2%;
+  margin-top:2%;
+ border-radius:50% ; 
+width: 70px;
+	float: left;
+}
+input:focus{
+		border-color:dodgerblue;
+}
+input {
+
+    width:120%;	
+    border: 2px solid;}
+body{
+	background-color: #632432;
+	font-family: Arial;
+}
+
+#main-container{
+	margin: 150px auto;
+	width: 600px;
+}
+header{
+  border:40px;
+}
+}
 
 </style>
 
-<div class="conatiner"><h2> Solicitud de prestamos</h2>
-<label style="color:grey; margin-left:5%;" ><span>Rellene los datos requeridos para solicitar su prestamo </span></label>
-<br>
-<h3>datos generales</h3>
-</div>
+<header>
+      <img src="https://conceptodefinicion.de/wp-content/uploads/2017/01/Educaci%C3%B3n_Ambiental.jpg"><h3>David jame</h3><label style="font-size:13px; color:grey;"><span>United states</label></span></label><br>
+      <label> Mobile: 871-567-4877</label>
+  </header>
+	<div id="main-container">
 
-<section class="container">
+		<table>
+			<thead>
+				<tr>
+					<th>Prestamos</th><th>Monto solicitado</th><th>Monto total</th><th>Indice de retrasos</th> <th>Calificacion</th>
+				</tr>
+			</thead>
 
- 
-<div class="container mt-2"> 
-   <div class="row">
-<div class="form-group col-md-6">
+			<tr>
+				<td>Agua</td><td>Enero</td><td>$ 3,000</td> <td>$ 3,000</td> <td>$ 3,000</td>
+			</tr>
+			<tr>
+				<td>Luz</td><td>Febrero</td><td>$ 5,500</td> <td>$ 3,000</td> <td>$ 3,000</td>
+			</tr>
+			<tr>
+				<td>Coche</td><td>Marzo</td><td>$ 10,100</td> <td>$ 3,000</td> <td>$ 3,000</td> 
+			</tr>
+			<tr>
+				<td>Internet</td><td>Abril</td><td>$ 450</td> <td>$ 3,000</td> <td>$ 3,000</td>  
+			</tr>
+		</table>
+	</div>
 
-<form data-toggle="boostrapValidate">
-
-<div class="form-group col-md-4">
-    <label >Monto solicitado </label>
-    <input type="number" id="monto" placeholder="Monto o cantidad" class="form-control" style="width:120%;" >
-</div>
-<div class="form-group col-md-4">
-    <label > Fecha</label>
-    <input type="date" id="fecha" placeholder="Fecha en que realizo el prestamo" class="form-control"style="width:115%; margin-right:20%;" >
-
-</div>
-<div class="form-group col-md-5">
-    <label style="margin-left:20px;"> Tiempo del prestamo </label>
-    <input type="week"  id="tiempo" placeholder="Tiempo de prestamo" class="form-control"  style="">
-</div>
-
-        
-<div class="form-group col-md-5">
-  
-    <label >Motivos por lo que solicita el prestamo </label><br>
-       <textarea id="descripcion" placeholder="escriba una breve descripcion" name="descripcion" rows="5" cols="
-       30" inputmode="full-width-latin" style="width:280%;"> </textarea>
-
-</div>
-
-<div class="form-group">
-<input class="btn btn-primary" type="submit" value="Enviar" id="enviar" style="width:40%; margin-inline-start: 50%;background:green;">  
-
-</div>
 </form>
 </div>
 
@@ -116,10 +224,15 @@ label{
 
 
 </div>
+ 
+
+    
+
 </body>
   
-  <script src="views/plugins/jQuery/jquery-2.2.3.min.js"></script>
-<script src="views/bootstrap/js/bootstrap.min.js"></script><script src="views/dist/js/app.min.js"></script>
+<script src="../plugins/jQuery/jquery-2.2.3.min.js"></script>
+<script src="../bootstrap/js/bootstrap.min.js"></script>
+<script src="../dist/js/app.min.js"></script>
 
 </body>
 </html>
