a15d2942cfcd27dc3ad3ce99d0c4e6b33edb255c
reimy <reimyreymm@gmail.com>
2019-04-12 16:56:03 -0400

    solicitud
    jjoj

--- views/Usuarios/Perfil.php
+++ views/Usuarios/Perfil.php
@@ -3,6 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <link rel="shortcut icon" type="image/png" href="../dist/img/logo1.png">
   <title>Sistema de financiamiento</title>
 
 <script defer src="https://use.fontawesome.com/releases/v5.8.1/js/all.js" integrity="sha384-g5uSoOSBd7KkhAMlnQILrecXvzst9TdC09/VM+pjDTCM+1il8RHz5fKANTFFb+gQ" crossorigin="anonymous"></script>
@@ -36,6 +37,14 @@
  border-color: red;
  color:white;
 }
+body {
+	height: 100%;
+	background-image: url("../img/fondo-login.jpg");
+	background-size: cover;
+	background-attachment: fixed;
+	
+}
+
 </style>
 <aside class="main-sidebar">
 
