9907f5e18690f9cba79e8cb7ced423f5157befa6
Aaron <aaronsenogara@gmail.com>
2019-04-12 00:21:16 -0400

    navbar temporal

--- views/Dashboard/sidebar.php
+++ views/Dashboard/sidebar.php
@@ -1,26 +1,13 @@
-<aside class="main-sidebar">
+   <!-- Left side column. contains the logo and sidebar 
+   <aside class="main-sidebar">
+                <section class="sidebar">
 
-  
-    <section class="sidebar"  >
+                    <ul class="sidebar-menu" data-widget="tree">
+                        <li class="active"><a href="../dashboard/solicitudpersonal.php"><i class="fa fa-link"></i> <span>Solicitud
+                                    personal</span></a></li>
+                        <li class="active"><a href="vistaclientes.php"><i class="fa fa-link"></i>
+                                <span>Clientes</span></a></li>
 
-  
-    
-        
-      </div>
- 
-
-      <ul class="sidebar-menu" >
-        <li class="header" style="margin-right:10px;;"><h1> <label style="margin-right:20px;">Impekable</label> </h1> </li>
-    
-        <li class="active"><a href="/SistemaPHP/views/historial.php"><i class="fas fa-home"></i>Home-calculadora </a></li>
-        <li><a href="#"><i class="fas fa-receipt"></i> <span>Historial de prestamos</span></a></li>
-         <li><a href="/SistemaPHP/views/template.php"><i class="fas fa-envelope"></i> <span>Solicitud de prestamos</span></a></li>
-        <li class="treeview">
-          <a href="#"><i class="fas fa-user"></i></i> <span>Perfil usuario </span>
-   
-        </li>
-      </ul>
-   
-    </section>
-
-  </aside>
+                    </ul>
+                </section>
+            </aside>  -->
